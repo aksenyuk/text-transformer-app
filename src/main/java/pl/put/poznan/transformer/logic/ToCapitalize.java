cc3fbbb418886de23f9a664872c92f9b3d3c935a
sonya <86928699+allsuitablenamesarealreadytaken@users.noreply.github.com>
2022-05-20 14:56:02 +0200

    Create ToCapitalize.java

---
+++ src/main/java/pl/put/poznan/transformer/logic/ToCapitalize.java
@@ -0,0 +1,23 @@
+package pl.put.poznan.transformer.logic;
+
+public class ToCapitalize extends Decorator{
+    
+    public ToCapitalize(TransformerInterface transformer){
+        super(transformer);
+    }
+    
+    public String transform(String text){
+        return capitalize(transformer.transform(text));
+    }
+
+    private String capitalize(String text){
+        list_text = text.split(" ");
+
+        for (int i = 0; i < len(list_text); i++){
+            list_text[i] = list_text[i].substring(0, 1).toUpperCase() + list_text[i].substring(1);
+        }
+        
+        text = String.join(" ", list_text);
+        return text;
+    }
+}
