c5981958f63bdeb28f48d6380098eece517abd7f
allsuitablenamesarealreadytaken <sofya.axenyuk.02@gmail.com>
2022-05-20 20:17:48 +0200

    EliminateRepetitions class documentation

--- src/main/java/pl/put/poznan/transformer/logic/EliminateRepetitions.java
+++ src/main/java/pl/put/poznan/transformer/logic/EliminateRepetitions.java
@@ -1,14 +1,35 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * The given class is created to remove repetitions in an input text
+ */
+
 public class EliminateRepetitions extends Decorator{
+
+    /**
+     * Decorator transformer constructor
+     * @param transformer - Decorator interface component of TransformerInterface type
+     */
 
     public EliminateRepetitions(TransformerInterface transformer){
         super(transformer);
     }
 
+    /**
+     * The method to activate this class text modifications
+     * @param text - an input text to be processed of String type
+     * @return modified text
+     */
+
     public String transform(String text){
         return eliminateRepetitions(transformer.transform(text));
     }
+
+    /**
+     * The method to remove repetitions in the parsed input text
+     * @param text - an input text to be processed of String type
+     * @return modified text
+     */
 
     private String eliminateRepetitions(String text){
         String tokenized[] = text.split(" ");
