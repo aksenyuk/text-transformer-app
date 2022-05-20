5753167a61ccab4d81002bc311f205b1f386d6d5
alessandro <vita02051802@gmail.com>
2022-05-20 16:55:11 +0200

    made Application work in CLI

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -2,11 +2,34 @@ package pl.put.poznan.transformer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.put.poznan.transformer.logic.TextTransformer;
+
+import java.util.Scanner;
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class TextTransformerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TextTransformerApplication.class, args);
+
+        System.out.println("Intro");
+
+        System.out.println("Input the text:");
+        Scanner sc= new Scanner(System.in);
+        String text = sc.nextLine();
+
+        System.out.println("Options");
+
+        String[] todo = sc.nextLine().split(";");
+        try {
+            TextTransformer transformer = new TextTransformer(todo);
+            text = transformer.transform(text);
+        }
+        catch(Exception ex){
+            System.out.println("ERROR!");
+        }
+
+        System.out.println("Transformed text:");
+        System.out.println(text);
     }
 }
