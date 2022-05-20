f08d2937d2c7baa63ddb5cc38673b0354bac98f8
YanaSolyah <giminosk@gmail.com>
2022-05-20 16:03:47 +0200

    Inverse()

---
+++ src/main/java/pl/put/poznan/transformer/logic/ToInverse.java
@@ -0,0 +1,34 @@
+package pl.put.poznan.transformer.logic;
+import java.util.*;
+import java.lang.*;
+public class ToInverse extends Decorator{
+
+    public ToInverse(TransformerInterface transformer){
+        super(transformer);
+    }
+
+    public String transform(String text){
+        return InverseFunction(transformer.transform(text));
+    }
+
+    private String InverseFunction(String text){
+        Set<Integer> IndexSet = new HashSet<Integer>();
+
+        for (int i = 0; i < text.length(); i++){
+            if (Character.isUpperCase(text.charAt(i))){
+                IndexSet.add(i);
+            }
+        }
+
+        StringBuilder text2 = new StringBuilder();
+        text2.append(text);
+        text2.reverse();
+        text = text2.toString().toLowerCase();
+        char[] text3 = text.toCharArray();
+        for (Integer idx : IndexSet){
+            text3[idx] = Character.toUpperCase(text3[idx]);
+        }
+        return text3.toString();
+    }
+
+}
