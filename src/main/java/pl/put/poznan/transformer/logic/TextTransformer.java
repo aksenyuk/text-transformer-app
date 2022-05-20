2b07c0d8727a835a1e8e92a4ab0e3ce06abc41d2
sonya <86928699+allsuitablenamesarealreadytaken@users.noreply.github.com>
2022-05-20 14:23:30 +0200

    Update TextTransformer.java

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -8,11 +8,42 @@ public class TextTransformer {
     private final String[] transforms;
 
     public TextTransformer(String[] transforms){
-        this.transforms = transforms;
+        // this.transforms = transforms;
+        switch (transforms) {
+            case "upper":
+                this.transforms = new ToUpper(transforms);
+                break;
+            case "lower":
+                this.transforms = new ToLower(transforms);
+                break;
+            case "capitalize":
+                this.transforms = new ToCapitalize(transforms);
+                break;
+            case "inverse":
+                this.transforms = new ToInverse(transforms);
+                break;
+            case "numbersToText":
+                this.transforms = new NumbersToText(transforms);
+                break;
+            case "stringsToAcronyms":
+                this.transforms = new ToAcronym(transforms);
+                break;
+            case "acronymsToStrings":
+                this.transforms = new ToFull(transforms);
+                break;
+            case "latex":
+                this.transforms = new ToLatex(transforms);
+                break;
+            case "removeRepetitions":
+                this.transforms = new removeRepetitions(transforms);
+                break;
+            default:
+                //throw new exception
+        }
     }
 
     public String transform(String text){
         // of course, normally it would do something based on the transforms
-        return text.toUpperCase();
+        // return text.toUpperCase();
     }
 }
